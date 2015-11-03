9de75cc879d66dc793db31277a35a210bae6c204
azoletwork <alessandro@ipsoft.it>
2015-11-03 11:16:38 +0100

    prepared for UMD (Universal Module Definition)

--- dist/leaflet-layer-opacity-control.js
+++ dist/leaflet-layer-opacity-control.js
@@ -1,71 +1,89 @@
-L.Control.layerOpacitySlider = L.Control.extend({
+// Following https://github.com/Leaflet/Leaflet/blob/master/PLUGIN-GUIDE.md
+(function (factory, window) {
 
-    options : {
-        position: 'bottomleft'
-    },
+    // define an AMD module that relies on 'leaflet'
+    if (typeof define === 'function' && define.amd) {
+        define(['leaflet'], factory);
 
-    initialize: function (options) {
-
-        // constructor
-        L.Util.setOptions(this, options);
-
-        this.layerList = [];
-
-    },
-
-    onAdd : function(map) {
-
-        console.info('Adding LayerOpacity.Control Control');
-
-        var that = this;
-
-        // create the control container with a particular class name
-        var container = L.DomUtil.create('div', 'layer-opacity-control leaflet-bar');
-
-        // ... initialize other DOM elements, add listeners, etc.
-        var sliderElement = L.DomUtil.create('input', 'slider', container);
-
-        var tooltipPosition = 'right';
-
-        if (that.options.position.indexOf('left') > -1) {
-            tooltipPosition = 'right';
-        } else {
-            tooltipPosition = 'left';
-        }
-
-        var slider = new Slider(sliderElement, {
-        //var slider = $(sliderElement).slider({
-            min : 0,
-            max : 100,
-            tooltip_position : tooltipPosition,
-            value : 100,
-            orientation : 'vertical'
-        });
-
-        slider.on('slideStart', function(value) {
-            map.dragging.disable();
-        });
-
-        slider.on('slideStop', function(value) {
-            map.dragging.enable();
-        });
-
-        slider.on('change', function(e) {
-            for (var i = 0, l = that.layerList.length; i < l; ++i) {
-                that.layerList[i].setOpacity(e.newValue / 100);
-            }
-        });
-
-        return container;
-
-    },
-
-    addLayer : function(layer) {
-
-        console.info('Adding layer to LayerOpacity.Control');
-
-        this.layerList.push(layer);
-
+    // define a Common JS module that relies on 'leaflet'
+    } else if (typeof exports === 'object') {
+        module.exports = factory(require('leaflet'));
     }
 
-});
+    // attach your plugin to the global 'L' variable
+    if (typeof window !== 'undefined' && window.L) {
+        window.L.Control.LayerOpacity = factory(L);
+        window.L.control.layerOpacity = function(options) {
+            return new window.L.Control.LayerOpacity(options);
+        };
+    }
+}(function (L) {
+
+	var LayerOpacity = L.Control.extend({
+            
+            options : {
+                position: 'topleft'
+            },
+
+            initialize: function (options) {
+
+                // constructor
+                L.Util.setOptions(this, options);
+
+            },
+
+            onAdd : function(map) {
+                console.info('Adding LayerOpacity.Control', this.options);
+
+                var that = this;
+
+                // create the control container with a specific class name
+                var container = L.DomUtil.create('div', 'layer-opacity-control leaflet-bar');
+
+                // ... initialize other DOM elements, add listeners, etc.
+                var sliderElement = L.DomUtil.create('input', 'slider', container);
+
+                var tooltipPosition = 'right';
+
+                if (that.options.position.indexOf('left') > -1) {
+                    tooltipPosition = 'right';
+                } else {
+                    tooltipPosition = 'left';
+                }
+
+                var slider = new Slider(sliderElement, {
+                    min : 0,
+                    max : 100,
+                    tooltip_position : tooltipPosition,
+                    value : 100,
+                    orientation : 'vertical'
+                });
+
+                slider.on('slideStart', function(value) {
+                    map.dragging.disable();
+                });
+
+                slider.on('slideStop', function(value) {
+                    map.dragging.enable();
+                });
+
+                slider.on('change', function(e) {
+                    if (that.options.layers) {
+                        for (var i = 0, l = that.options.layers.length; i < l; ++i) {
+                            that.options.layers[i].setOpacity(e.newValue / 100);
+                        }
+                    }
+                });
+
+                return container;
+            },
+
+            addLayer : function(layer) {
+                console.info('Adding layer to LayerOpacity.Control');
+                this.layers.push(layer);
+            }
+	});
+	
+	return LayerOpacity;
+	
+}, window));
